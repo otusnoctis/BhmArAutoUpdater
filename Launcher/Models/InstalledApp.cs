dcf39b36e1c766ac261df3ecec1d005e6a8c4b3e
otusnoctis <otus.noctis@proton.me>
2026-04-04 21:47:32 +0200

    Remove obsolete launcher project

--- Launcher/Models/InstalledApp.cs
+++
@@ -1,9 +0,0 @@
-namespace Launcher.Models;
-
-public sealed class InstalledApp
-{
-    public required string DisplayName { get; init; }
-    public required string FolderName { get; init; }
-    public required string ExecutablePath { get; init; }
-    public required Version Version { get; init; }
-}
